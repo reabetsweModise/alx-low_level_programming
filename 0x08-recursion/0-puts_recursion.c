95a6f84782d7bd87ab245d1a9876b75dc4749978
reabetsweModise <https://github.com/reabetsweModise/0x08-recursion.git>
2023-06-07 12:19:22 -0700

    fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
-
 /**
-*_my_puts_recursion - prints a string recursively
-*@s: input string
-*Return: void
-*/
-void _my_puts_recursion(char *s)
+ *_puts_recursion - works like puts function;
+ *@s: input
+ * Return:0 (Success)
+ */
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_my_putchar(*s);
-		_my_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	{
-		_my_putchar('\n');
-	}
+		_putchar('\n');
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,14 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _my_puts_recursion(char *s);
-int _my_putchar(char c);
-void _my_print_rev_recursion(char *s);
-int _my_strlen_recursion(char *s);
-int my_factorial(int n);
-int _my_pow_recursion(int x, int y);
-int _my_sqrt_recursion(int n);
-int my_is_prime_number(int n);
-int my_is_palindrome(char *s);
-
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
 #endif
